8d882551e7b7b659dfb9aebfc1acdd2707dd095d
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-06-14 22:02:54 +0530

    show issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const loadingIssueDetailPage = () => {
+  return <div>loading...</div>;
+};
+
+export default loadingIssueDetailPage;
---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { prisma } from "@/prisma/client";
+import { notFound } from "next/navigation";
+import React from "react";
+
+interface issueDetailProps {
+  params: { id: string };
+}
+
+const IssueDetailPage = async ({ params: { id } }: issueDetailProps) => {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!issue) return notFound();
+
+  return (
+    <div>
+      <p>{issue.title}</p>
+      <p>{issue.description}</p>
+      <p>{issue.status}</p>
+      <p>{issue.createdAt.toDateString()}</p>
+    </div>
+  );
+};
+
+export default IssueDetailPage;
